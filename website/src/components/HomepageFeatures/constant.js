bd5650962005513eb92fe1593e34fcbdfc2be229
billchen <yibie_jingnian@163.com>
2024-03-25 16:14:13 +0800

    feat: add Tabs

---
+++ website/src/components/HomepageFeatures/constant.js
@@ -0,0 +1,18 @@
+export const featureList = [
+  {
+    title: "Automated and effortless RAG workflow",
+    items: [],
+  },
+  {
+    title: "From 'garbage in, garbage out' to 'quality in, quality out'",
+    items: [],
+  },
+  {
+    title: "Excellent compatibility with heterogeneous data sources",
+    items: [],
+  },
+  {
+    title: "Grounds and citations for reduced hallucinations",
+    items: [],
+  },
+];
