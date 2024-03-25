000e1b41ff411b20d9e42e06aa1665ec7066778d
balibabu <cike8899@users.noreply.github.com>
2024-03-25 19:27:40 +0800

    feat: add background image to header and replace github icon (#2)
    * feat: replace github icon
    
    * feat: add background image to header

--- website/src/components/HomepageFeatures/constant.js
+++ website/src/components/HomepageFeatures/constant.js
@@ -1,18 +1,106 @@
 export const featureList = [
   {
     title: "Automated and effortless RAG workflow",
-    items: [],
+    items: [
+      {
+        key: 1,
+        description:
+          "Streamlined RAG orchestration catered to both personal and large businesses",
+        icon: require("@site/static/img/tab.svg").default,
+        children: require("@site/static/img/tab-content-01.svg").default,
+      },
+      {
+        key: 2,
+        description: "Configurable LLMs as well as embedding models",
+        icon: require("@site/static/img/tab.svg").default,
+        children: "image2",
+      },
+      {
+        key: 3,
+        description: "Multiple recall paired with fused reranking",
+        icon: require("@site/static/img/tab.svg").default,
+        children: "image3",
+      },
+      {
+        key: 4,
+        description: "Intuitive APIs for seamless integration with business",
+        icon: require("@site/static/img/tab.svg").default,
+        children: "image4",
+      },
+    ],
   },
   {
     title: "From 'garbage in, garbage out' to 'quality in, quality out'",
-    items: [],
+    items: [
+      {
+        key: 1,
+        description:
+          "Deep document understanding-based knowledge extraction from unstructured data of complicated formats",
+        icon: require("@site/static/img/tab.svg").default,
+        children: "image1",
+      },
+      {
+        key: 2,
+        description:
+          "Text chunking based on multiple template options: intellegent and also explainable",
+        icon: require("@site/static/img/tab.svg").default,
+        children: "image2",
+      },
+      {
+        key: 3,
+        description:
+          "Finds 'needle in a data haystack' of literally unlimited tokens",
+        icon: require("@site/static/img/tab.svg").default,
+        children: "image3",
+      },
+    ],
   },
   {
     title: "Excellent compatibility with heterogeneous data sources",
-    items: [],
+    items: [
+      {
+        key: 1,
+        description:
+          "Documents spanning Word, slides, excel, txt, images, and scanned copies.",
+        icon: require("@site/static/img/tab.svg").default,
+        children: "image1",
+      },
+      {
+        key: 2,
+        description: "Structured data ",
+        icon: require("@site/static/img/tab.svg").default,
+        children: "image2",
+      },
+      {
+        key: 3,
+        description: "Web pages and more to come.",
+        icon: require("@site/static/img/tab.svg").default,
+        children: "image3",
+      },
+    ],
   },
   {
     title: "Grounds and citations for reduced hallucinations",
-    items: [],
+    items: [
+      {
+        key: 1,
+        description:
+          "Visualization of text chunking to allow human intervention.",
+        icon: require("@site/static/img/tab.svg").default,
+        children: "image1",
+      },
+      {
+        key: 2,
+        description: "Quick view of the key references in a given answer.",
+        icon: require("@site/static/img/tab.svg").default,
+        children: "image2",
+      },
+      {
+        key: 3,
+        description: "Traceable citations to support grounded-answers.",
+        icon: require("@site/static/img/tab.svg").default,
+        children: "image3",
+      },
+    ],
   },
 ];
--- website/src/components/HomepageFeatures/tabs/index.jsx
+++ website/src/components/HomepageFeatures/tabs/index.jsx
@@ -3,36 +3,9 @@ import clsx from "clsx";
 
 import styles from "./styles.module.css";
 
-const list = [
-  {
-    key: "1",
-    description:
-      "Streamlined RAG orchestration catered to both personal and large businesses",
-    icon: require("@site/static/img/tab.svg").default,
-    children: "image1",
-  },
-  {
-    key: "2",
-    description: "Configurable LLMs as well as embedding models",
-    icon: require("@site/static/img/tab.svg").default,
-    children: "image2",
-  },
-  {
-    key: "3",
-    description: "Multiple recall paired with fused reranking",
-    icon: require("@site/static/img/tab.svg").default,
-    children: "image3",
-  },
-  {
-    key: "4",
-    description: "Intuitive APIs for seamless integration with business",
-    icon: require("@site/static/img/tab.svg").default,
-    children: "image4",
-  },
-];
-
-const Tabs = ({ items = list }) => {
-  const [selectedKey, setSelectedKey] = useState("1");
+const Tabs = ({ items = [] }) => {
+  const [selectedKey, setSelectedKey] = useState(1);
+  const Child = items.find((x) => x.key === selectedKey)?.children;
 
   const handleClick = (key) => () => {
     setSelectedKey(key);
@@ -60,7 +33,11 @@ const Tabs = ({ items = list }) => {
         })}
       </section>
       <section className={styles.tabChildren}>
-        {items.find((x) => x.key === selectedKey).children}
+        {typeof Child === "string" ? (
+          Child
+        ) : (
+          <Child className={styles.tabChild} width="100%"></Child>
+        )}
       </section>
     </section>
   );
