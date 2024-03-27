646be9ebf1534b08a83e884e5806c2d0a463c2b6
balibabu <cike8899@users.noreply.github.com>
2024-03-27 12:09:18 +0800

    feat: add slider (#6)

--- website/src/components/HomepageFeatures/constant.js
+++ website/src/components/HomepageFeatures/constant.js
@@ -1,109 +1,135 @@
 export const featureList = [
   {
     title: `From "garbage in, garbage out" to "quality in, quality out"`,
-    img: "phase2.gif",
     items: [
       {
         key: 1,
         description:
           "Deep document understanding-based knowledge extraction from unstructured data of complicated formats",
-        icon: require("@site/static/img/tab.svg").default,
+        icon: "gigo-chunk-list.png",
         children: "image1",
       },
       {
         key: 2,
         description:
           "Text chunking based on multiple template options: intellegent and also explainable",
-        icon: require("@site/static/img/tab.svg").default,
+        icon: "gigo-chunk-list1.png",
         children: "image2",
       },
       {
         key: 3,
         description: `Finds "needle in a data haystack" of literally unlimited tokens`,
-        icon: require("@site/static/img/tab.svg").default,
+        icon: "gigo-retieval-test.png",
         children: "image3",
       },
     ],
   },
   {
     title: "Grounds and citations for reduced hallucinations",
-    img: "phase4.gif",
     items: [
       {
         key: 1,
         description:
           "Visualization of text chunking to allow human intervention.",
-        icon: require("@site/static/img/tab.svg").default,
+        icon: "ground-chat.png",
         children: "image1",
       },
       {
         key: 2,
         description: "Quick view of the key references in a given answer.",
-        icon: require("@site/static/img/tab.svg").default,
+        icon: "ground-chat1.png",
         children: "image2",
       },
       {
         key: 3,
         description: "Traceable citations to support grounded-answers.",
-        icon: require("@site/static/img/tab.svg").default,
+        icon: "ground-chat3.png",
+        children: "image3",
+      },
+      {
+        key: 4,
+        description: "Traceable citations to support grounded-answers.",
+        icon: "ground-dialog-conf.png",
         children: "image3",
       },
     ],
   },
   {
     title: "Excellent compatibility with heterogeneous data sources",
-    img: "phase3.gif",
     items: [
       {
         key: 1,
         description:
           "Documents spanning Word, slides, excel, txt, images, and scanned copies.",
-        icon: require("@site/static/img/tab.svg").default,
+        icon: "excellent-doc-list.png",
         children: "image1",
       },
       {
         key: 2,
         description: "Structured data ",
-        icon: require("@site/static/img/tab.svg").default,
+        icon: "excellent-kb-config1.png",
         children: "image2",
       },
       {
         key: 3,
         description: "Web pages and more to come.",
-        icon: require("@site/static/img/tab.svg").default,
+        icon: "excellent-kb-config2.png",
+        children: "image3",
+      },
+      {
+        key: 4,
+        description: "Web pages and more to come.",
+        icon: "excellent-kb-config3.png",
+        children: "image3",
+      },
+      {
+        key: 5,
+        description: "Web pages and more to come.",
+        icon: "excellent-kb-config4.png",
+        children: "image3",
+      },
+      {
+        key: 6,
+        description: "Web pages and more to come.",
+        icon: "excellent-kb-config5.png",
+        children: "image3",
+      },
+      {
+        key: 7,
+        description: "Web pages and more to come.",
+        icon: "excellent-kb-config6.png",
+        children: "image3",
+      },
+      {
+        key: 8,
+        description: "Web pages and more to come.",
+        icon: "excellent-kb-config7.png",
         children: "image3",
       },
     ],
   },
   {
     title: "Automated and effortless RAG workflow",
-    img: "phase1.gif",
     items: [
       {
         key: 1,
         description:
           "Streamlined RAG orchestration catered to both personal and large businesses",
-        icon: require("@site/static/img/tab.svg").default,
-        // children: require("@site/static/img/tab-content-01.svg").default,
+        icon: "automated-knowledgebase-list.png",
+        // children: "tab-content-01.svg",
         children: "image1",
       },
       {
         key: 2,
         description: "Configurable LLMs as well as embedding models",
-        icon: require("@site/static/img/tab.svg").default,
+        icon: "automated-setting.png",
         children: "image2",
       },
       {
         key: 3,
         description: "Multiple recall paired with fused reranking",
-        icon: require("@site/static/img/tab.svg").default,
+        icon: "automated-setting1.png",
         children: "image3",
-      },
-      {
-        key: 4,
-        description: "Intuitive APIs for seamless integration with business",
-        icon: require("@site/static/img/tab.svg").default,
-        children: "image4",
       },
     ],
   },
