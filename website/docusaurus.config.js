cca56207a240a8b5ac638cbdcccae766cfa2ee0c
Vissidarte-Herman <cai.keith@gmail.com>
2024-06-19 12:57:37 +0800

    Updated social card

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/ragflow-social-card.png",
       colorMode: {
         defaultMode: 'light',
         disableSwitch: true,
