d88fc306807841082f3e8ea411981f5ccc35e0b8
writinwaters <93570324+writinwaters@users.noreply.github.com>
2024-09-11 20:00:11 +0800

    Update githubStar.tsx

--- website/src/components/NavbarItems/githubStar.tsx
+++ website/src/components/NavbarItems/githubStar.tsx
@@ -23,7 +23,7 @@ export const GithubStars = () => {
           if (data.stargazers_count) {
             stars = (data.stargazers_count / 1000).toFixed(1) + "k";
           } else {
-            stars = "15k";
+            stars = "20k";
           }
           globalstars = stars;
           setStar(stars);
