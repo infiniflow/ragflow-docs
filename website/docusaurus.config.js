f2b89e7c0640d5de5a3615814fb9a140ed9ef9f4
writinwaters <cai.keith@gmail.com>
2024-09-19 18:35:52 +0800

    Added blog explaining GraphRAG

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -36,6 +36,19 @@ const config = {
     locales: ["en"],
   },
 
+  plugins: [
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 500, // max resized image's size.
+        min: 300, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        disableInDev: false,
+      },
+    ],
+  ],
+
   presets: [
     [
       "classic",
