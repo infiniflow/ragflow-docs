deb4bb6f0f57b4f5f58fb773407e1fd95cbe02fa
writinwaters <cai.keith@gmail.com>
2024-11-26 14:47:51 +0800

    Resolved a broken link

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -5,6 +5,7 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import { themes as prismThemes } from "prism-react-renderer";
+import path from "path";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
