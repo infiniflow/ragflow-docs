77e77db0d238eaa9a378937e770de203986430c0
writinwaters <cai.keith@gmail.com>
2024-11-26 15:08:27 +0800

    minor

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -5,7 +5,6 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import { themes as prismThemes } from "prism-react-renderer";
-import path from "path";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
